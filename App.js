cb95b7cf021ffcd0e58772a3b9e0c0d7a7d0b353
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-02-22 12:52:47 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View, Alert, Switch } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Ronny!</Text>
+			<Text style={styles.headingText}>What is going on!?</Text>
+			<Button
+				title='Press Me'
+				style={styles.button}
+				onPress={() => Alert.alert('You pressed the button!')}
+				accessibilityLabel={'Press this button to popup an alert'}
+			/>
+			<StatusBar style='auto' />
+      <Switch />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		// alignItems: 'center',
+		margin: 10,
+		justifyContent: 'center',
+	},
+	headingText: {
+		fontSize: 36,
+	},
+	button: {
+		backgroundColor: 'yellow',
+		padding: 20,
+	},
+})
